b8a8faded5e30af99060a0eab93d3f9c80c6334e
Karol Kutrowski <kutrowski@heavy-tuned.de>
2024-07-03 14:53:24 +0200

    build

--- src/utils/audioRecording.ts
+++ src/utils/audioRecording.ts
@@ -50,46 +50,8 @@ export function startAudioRecording(
         }
       }
 
-      console.log(error);
+      console.error(error);
 
-      //Error handling structure
-      switch (error.name) {
-        case 'AbortError': //error from navigator.mediaDevices.getUserMedia
-          // eslint-disable-next-line no-console
-          console.log('An AbortError has occurred.');
-          break;
-        case 'NotAllowedError': //error from navigator.mediaDevices.getUserMedia
-          // eslint-disable-next-line no-console
-          console.log('A NotAllowedError has occurred. User might have denied permission.');
-          break;
-        case 'NotFoundError': //error from navigator.mediaDevices.getUserMedia
-          // eslint-disable-next-line no-console
-          console.log('A NotFoundError has occurred.');
-          break;
-        case 'NotReadableError': //error from navigator.mediaDevices.getUserMedia
-          // eslint-disable-next-line no-console
-          console.log('A NotReadableError has occurred.');
-          break;
-        case 'SecurityError': //error from navigator.mediaDevices.getUserMedia or from the MediaRecorder.start
-          // eslint-disable-next-line no-console
-          console.log('A SecurityError has occurred.');
-          break;
-        case 'TypeError': //error from navigator.mediaDevices.getUserMedia
-          // eslint-disable-next-line no-console
-          console.log('A TypeError has occurred.');
-          break;
-        case 'InvalidStateError': //error from the MediaRecorder.start
-          // eslint-disable-next-line no-console
-          console.log('An InvalidStateError has occurred.');
-          break;
-        case 'UnknownError': //error from the MediaRecorder.start
-          // eslint-disable-next-line no-console
-          console.log('An UnknownError has occurred.');
-          break;
-        default:
-          // eslint-disable-next-line no-console
-          console.log('An error occurred with the error name ' + error.name);
-      }
     });
 }
 /** Stop the currently started audio recording & sends it
